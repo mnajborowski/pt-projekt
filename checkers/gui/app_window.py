7b496a02e0a68c60039b141dfa5bdadd89a4eb95
Adrian Glapiński <adriangl1997@gmail.com>
2020-04-26 21:45:13 +0200

    Add thread support

--- checkers/gui/app_window.py
+++ checkers/gui/app_window.py
@@ -1,9 +1,10 @@
 import sys
 
 import numpy as np
+from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QIcon
 from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout)
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout
 
 test_matrix = np.array(
     [[0, 0, 1, 1, 0, 0, 0, 0],
@@ -24,14 +25,30 @@ test_matrix2 = np.array(
      [0, 2, 0, 2, 0, 2, 0, 2],
      [0, 2, 0, 2, 0, 2, 0, 2],
      [0, 2, 0, 2, 0, 2, 0, 2],
-     [0, 2, 0, 2, 0, 2, 0, 2], ],
+     [0, 2, 0, 2, 0, 2, 0, 2]],
     dtype=int
 )
+
+
+class Worker(QObject):
+    new_board_ready = pyqtSignal(np.ndarray)
+
+    @pyqtSlot(np.ndarray)
+    def emit_new_board(self, board):
+        self.new_board_ready.emit(board)
 
 
 class AppWindow(QWidget):
     def __init__(self):
         super().__init__()
+        # Create Worker and Thread
+        self.worker = Worker()
+        self.thread = QThread()
+        self.worker.new_board_ready.connect(self.draw_checkerboard)
+        self.worker.moveToThread(self.thread)
+        self.thread.start()
+
+        # Init UI
         self.setWindowIcon(QIcon('assets/app_icon.png'))
         self.setMinimumSize(640, 640)
 
@@ -42,6 +59,7 @@ class AppWindow(QWidget):
         self.draw_checkerboard()
 
     def draw_checkerboard(self, board_matrix=None):
+        self.__clear_grid_layout()
         if board_matrix is None:
             for x in range(8):
                 for y in range(8):
@@ -51,7 +69,6 @@ class AppWindow(QWidget):
                         square = QSvgWidget('assets/light_square.svg')
                     self.grid_layout.addWidget(square, x, y)
         else:
-            self.__clear_grid_layout()
             for x in range(8):
                 for y in range(8):
                     if (x * 7 + y) % 2:
@@ -84,7 +101,7 @@ if __name__ == '__main__':
 
     app_window = AppWindow()
     app_window.show()
-    app_window.draw_checkerboard(test_matrix2)
-    app_window.draw_checkerboard(test_matrix)
+    app_window.worker.emit_new_board(test_matrix2)
+    app_window.worker.emit_new_board(test_matrix)
 
     sys.exit(app.exec_())
