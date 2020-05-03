dd0ab0b58ad972af8eb68234f679bc2929f239fb
mnajborowski <michal.najborowski@gmail.com>
2020-05-03 03:28:10 +0200

    move check

--- checkers/image/pawncolours.py
+++ checkers/image/pawncolours.py
@@ -2,6 +2,14 @@ from enum import Enum
 
 
 class PawnColour(Enum):
+    EMPTY = 0
     BLACK = 1
     WHITE = 2
     UNDEFINED = 3
+
+
+def opposite(color):
+    if color == PawnColour.BLACK:
+        return PawnColour.WHITE
+    else:
+        return PawnColour.BLACK
